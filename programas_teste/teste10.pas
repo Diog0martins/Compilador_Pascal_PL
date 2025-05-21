a3fdda4346158ed7fafd34b7075cb7a48073ed5d
a104360 <a104360@alunos.uminho.pt>
2025-05-21 02:01:51 +0100

    Ciclos for simples

---
+++ programas_teste/teste10.pas
@@ -0,0 +1,9 @@
+program CountUp;
+var
+  i: integer;
+begin
+  for i := 1 to 5 do
+  begin
+    i:=i;
+  end;
+end.
--- src/parser/loops.py
+++ src/parser/loops.py
@@ -1,5 +1,6 @@
 from loops_table import counter
 
+import re
 """
 VERIFICAR SE É POSSÍVEL TROCAR POR BLOCO DO MAIN
 """
@@ -41,12 +42,65 @@ def p_ciclo_for(p):
     '''
     CicloFor : FOR Atribuicao DirecaoFor Expressao DO LocalInstsList
     '''
-    p[0] = f"{p[1]} {p[2]} {p[3]} {p[4]} {p[5]} {p[6]}"
-    print("Ciclo FOR reconhecido")
+    start_label = f'FORSTART{counter.get_for()}'
+    end_label = f'FOREND{counter.get_for()}'
+    print(start_label)
+    counter.inc_for()
+    setup_variable = p[2]
+    print('#########################')
+    print(setup_variable)
+    print('#########################')
+
+
+    increment_position = re.findall(r'STOREG (\d+)',setup_variable)[0]
+
+    set_limit = f"PUSHI {p[4][0]}"
+
+    operation = p[3]
+
+    behavior = p[6]
+
+    p[0] = '\n'.join([
+        # atribuicao do valor do iterador
+        setup_variable,
+        # inicio do ciclo 
+        "START",
+        # definicao do limite
+        set_limit,
+        # rotulo de inicio
+        f'{start_label}:',
+        # verificacao
+        "PUSHL 0",
+        f"PUSHG {increment_position}",
+        "EQUAL",
+        "NOT",
+        f"JZ {end_label}",
+        # corpo
+        behavior,
+        # alterar iterador
+        f'PUSHG {increment_position}',
+        'PUSHI 1',
+        operation,
+        f'STOREG {increment_position}',
+        f'JUMP {start_label}',
+        # rotulo de fim de ciclo
+        f'{end_label}:',
+        # retiro da variável de limite
+        'POP 1',
+    ])
+
+    # p[0] = f"{p[1]} {p[2]} {p[3]} {p[4]} {p[5]} {p[6]}"
+    # print("Ciclo FOR reconhecido")
 
 def p_direcao_for(p):
     '''
     DirecaoFor : TO
                | DOWNTO
     '''
-    print(f"Direção do FOR: {p[1]}")
+    # print(f"Direção do FOR: {p[1]}")
+    if p[1] == 'to':
+        print('TOTO AFRICA')
+        p[0] = 'ADD'
+    if p[1] == 'downto':
+        print('SINDROME DOWN')
+        p[0] = 'SUB'
